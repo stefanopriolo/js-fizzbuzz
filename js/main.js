ba56d188b7bd8f594c2db56ddf2a98b472953bf3
stefanopriolo <stefanojpriolo@gmail.com>
2023-07-04 17:41:03 +0200

    css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
+const fizzbuzz = document.getElementById("fizzbuzz");
+
 for (let i = 1; i <= 100; i++) {
   let fizz = "Fizz";
   let buzz = "FizzBuzz";
   let numb = i;
+
+  const box = document.createElement("div");
+  // <div></div>
+
+  box.classList.add("box");
+  // <div class="box"></div>
+
   if (i % 3 === 0) {
     console.log(fizz);
+    box.innerHTML = fizz;
+    box.classList.add("green");
+    // <div class="box">fizz</div>
   } else if (i % 5 === 0) {
     console.log(buzz);
+    box.innerHTML = numb;
+    box.classList.add("box");
+    // <div class="box">numb</div>
   } else {
     console.log(numb);
+    box.innerHTML = numb;
+    box.classList.add("box");
+    // <div class="box">numb</div>
   }
+
+  fizzbuzz.append(box);
 }
